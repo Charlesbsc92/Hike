6d7a9ea1fa8a6c6cb805eeb51b07e27e974f19d6
Charles <charles@Charless-Mac-mini.local>
2025-01-21 22:16:23 +0530

    Added Project files

---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Charles on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
